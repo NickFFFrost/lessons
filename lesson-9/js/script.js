f5f8af9e539fa8335de89cf90c9d66ca9c39c078
nikita <nelsooooooon@gmail.com>
2019-05-14 00:03:23 +0300

    lesson

--- lesson-9/js/script.js
+++ lesson-9/js/script.js
@@ -97,20 +97,25 @@ setClock ("timer", deadline);
 
 let more = document.querySelector(".more"),
     overlay = document.querySelector(".overlay"),
-    close = document.querySelector(".popup-close");
+    close = document.querySelector(".popup-close"),
+    descrBtn = document.getElementsByClassName("description-btn");
 
-more.addEventListener("click", function () {
-  overlay.style.display = "block";
-  this.classList.add("more-splash");
-  document.body.style.overflow = "hidden";
-});
+  function openModal () {
+    overlay.style.display = "block";
+    this.classList.add("more-splash");
+    document.body.style.overflow = "hidden";
+  }
+  more.addEventListener("click", openModal);
+  
+  function closeModal () {
+    overlay.style.display = "none";
+    more.classList.remove("more-splash");
+    document.body.style.overflow = "";
+  }
+  close.addEventListener("click", closeModal);
 
-close.addEventListener("click", function () {
-  overlay.style.display = "none";
-  more.classList.remove("more-splash");
-  document.body.style.overflow = "";
-});
-
-
+  for (let i = 0; i < descrBtn.length; i++) {
+    descrBtn[i].addEventListener("click", openModal);
+  }
 
 });
